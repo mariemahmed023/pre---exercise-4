4d28757a3acfae8355e33656f0d1bf2e3c294b45
mariemahmed023 <maryamahmedelsayedshatah124@gmail.com>
2025-08-21 20:43:48 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,21 +1,46 @@
 let popUpEle = document.querySelector(".popup"),
     popupKeys = document.querySelectorAll("#Gallery .item i"),
-    popUpClose =  document.querySelector(".close");
+    popUpClose = popUpEle.querySelector(".close"),
+    popupImg = popUpEle.querySelector("img"),
+    currentImgIndex,
+    popupIndicators = popUpEle.querySelectorAll(".indicators li");
+
+//add show to the popup
+function togglePopup() {
+    popUpEle.classList.toggle("show");
+}
+
+function updateIndicator() {
+    document.querySelector(".indictors ul li.active").classList.remove("active");
+    popupIndicators[currentImgIndex].classList.add("active");
+}
 
 //popup is activated
+popupKeys.forEach(function (popupKey) {
+    popupKey.addEventListener("click", function (e) {
+        //As soon as i click on the plus icon the images are displayed
+        let currentImgEle = popupKey.parentElement.previousElementSibling,
+            currentImgSrc = currentImgEle.getAttribute("src");
+        // add active to the desired image
+        currentImgIndex = currentImgEle.dataset.index;
+        updateIndicator();
 
-popupKeys.forEach(function (popupKey){
-    popupKey.addEventListener("click", function () {
-        popUpEle.classList.add("show");
+
+        popupImg.setAttribute("src", currentImgSrc);
+        togglePopup();
     });
 });
+//popup is hidden
+popUpClose.addEventListener("click", togglePopup);
 
-//popup is closed
+//close the popup when I click outside the container of popup
+popUpEle.addEventListener("click", togglePopup);
 
-popupKeys.forEach(function (popupKey){
-    popupKey.addEventListener("click", function () {
-        popUpEle.classList.remove("show"); 
-    });
+//stop the function of closing popup
+popUpEle.querySelector(".box").addEventListener("click", function (e) {
+    e.stopPropagation();
 });
+
+
 
 
